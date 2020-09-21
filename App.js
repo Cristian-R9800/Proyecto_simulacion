f7b1ac1c6cacd798b0df2e7d7d346ed0f8e74dd9
Cristian-R9800 <39307896+Cristian-R9800@users.noreply.github.com>
2020-09-21 13:55:17 -0500

    Preuba

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput,Button,Image,Alert} from 'react-native';
+const imagen =  require("./assets/ruleta.jpg")
+let estrategia = {
+  name: ""
+}
+
+const nameHandeler = e =>{
+  estrategia.name= e.target.value
+}
+
+const mostrarMensaje = () =>{
+  Alert.alert(
+    "Alert Title",
+    "My Alert Msg",
+    [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") }
+    ],
+    { cancelable: false }
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! JAJA it's not working puto </Text>
+      <Text>Simulated Anniling Aplicado a “SO WHO’S COUNTING”</Text>
+      <Text>{estrategia.name}</Text>
+      <Text> Lina Maria Bocanegra</Text>
+      <Text> Cristian Felipe Ramirez Franco </Text>
+      <Image source={require('./assets/ruleta.jpg')}/>
+
+      <TextInput type="number" onChange={nameHandeler} value= {estrategia.name} placeholder= "Ingrese el numero"/>
+       <Button onPress={mostrarMensaje} title="Mostro"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +45,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color:"red",
+    backgroundColor: '#1565c0',
     alignItems: 'center',
     justifyContent: 'center',
   },
